b1c7202f083697ba1590b0bc2a923c3175875edb
Nathan Green <nathan@nathangreen.me>
2020-09-17 09:52:41 +0100

    Add menu & quit gracefully

--- airport.py
+++ airport.py
@@ -1,9 +1,40 @@
 import csv
+
+# Open Airports.txt and load into variable 'airports'
 airportsCSV = open('Airports.txt', 'r')
-#with open('Airports.txt', 'r') as csvfile:
-reader = csv.reader(airportsCSV)
+csvreader = csv.reader(airportsCSV)
 airports = []
-for row in reader:
+for row in csvreader:
     airports.append(row)
 
-print(airports)
+
+# Create menu
+def mainMenu():
+    print("=========")
+    print("Main Menu")
+    print("=========")
+    print("1. Enter airport details")
+    print("2. Enter flight details")
+    print("3. Enter price plan & calculate profit")
+    print("4. Clear data")
+    print("5. Quit")
+    try:
+        option = input("Please choose an option: [1-5] ")
+        option = int(option[0])
+        if option < 1 or option > 5:
+            raise ValueError
+    except SystemExit:
+        print("Goodbye :)")
+    except:
+        print("Input was invalid.")
+        mainMenu()
+
+    if option == 5:
+        # exit gracefully
+        try:
+            quit()
+        except SystemExit:
+            print("Goodbye :)")
+
+
+mainMenu()
