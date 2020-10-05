852553f357abba3ca6f3ed64f1228b31ff6025a9
Nathan Green <nathan@nathangreen.me>
2020-10-05 23:14:05 +0100

    Get flight details and add comments

--- airport.py
+++ airport.py
@@ -1,12 +1,25 @@
+#####################################
+##                                 ##
+##   Airport NEA by Nathan Green   ##
+##                                 ##
+#####################################
+
 import csv
 from time import sleep
 
 # Open Airports.txt and load into variable 'airports'
-airportsCSV = open('Airports.txt', 'r')
-csvreader = csv.reader(airportsCSV)
-airports = []
-for row in csvreader:
-    airports.append(row)
+airportsCSV = open('Airports.txt', 'r') # Open file
+csvreader = csv.reader(airportsCSV) # Load file into CSV module
+airports = [] # Prepare empty array
+for row in csvreader: # Loop over rows
+    airports.append(row) # Add rows to array
+
+aircrafts = [['Medium narrow body', 8, 2650, 180, 8],
+             ['Large narrow body', 7, 5600, 220, 10],
+             ['Medium wide body', 5, 4050, 406, 14]]
+
+airportDetails = ''
+flightDetails = ''
 
 
 # Create menu
@@ -31,33 +44,77 @@ def mainMenu():
 
     if option == 1:
         airportDetails = getAirportDetails(airports)
+    if option == 2:
+        flightDetails = getFlightDetails(aircrafts)
     if option == 5:
         return "quit"
 
 
 def getAirportDetails(airportData):
-    try:
+    try: # Get first 3 letters of input and make uppercase.
         ukAirport = input("Enter a UK airport code [LPL/BOH]>> ")[0:3].upper()
         if ukAirport != "LPL" and ukAirport != "BOH":
             raise ValueError
         overseasAirport = input(
             "Enter an overseas airport code >> ")[0:3].upper()
         isValid = False
-        for airport in airportData:
+        for airport in airportData: # Search airportData for code
             if overseasAirport == airport[0]:
                 isValid = True
                 print(airport[1] + " has been selected.")
-        if isValid == False:
+        if isValid == False: # Code wasn't found in airportData
             print("No valid airport was entered. Returning to menu.")
+    except: # Handle errors from invalid input
+        print("Input was invalid. Returning to menu.")
+
+
+def getFlightDetails(aircraftData):
+    print("Available aircraft types:")
+    for count, aircraft in enumerate(aircraftData): # Loop over aircraft
+        print(str(count + 1) + ". " + aircraft[0]) # Print numbered list of aircraft
+    try: # Handle inputs as in mainMenu
+        option = input("Please choose an option: [1-" +
+                       str(len(aircraftData)) + "] ") # Dynamically change max input
+        option = int(option[0])
+        if option < 1 or option > len(aircraftData):
+            raise ValueError
     except:
+        print("Input was invalid. Returning to menu.")
+
+    labels = [
+        "Type", "Running cost/seat/100km", "Max flight range (km)",
+        "Capacity (all standard seats)", "Min 1st class seats"
+    ]
+    aircraft = aircraftData[option - 1] # Store selected aircraft
+    print("\nAircraft selected:")
+    for count, detail in enumerate(aircraft): # Loop over selected aircraft
+        print(labels[count] + ": " + str(detail)) # Print details of aircraft
+
+    try:
+        option = input("\nEnter number of 1st class seats: (" +
+                       str(aircraft[4]) + " - " + str(int(aircraft[3] / 2)) + ") ") # Calculate seat ranges
+        option = int(option)
+        if option < aircraft[4]: # Raise error if too low
+            lowHigh = "low"
+            raise ValueError
+        elif option > aircraft[3] / 2: # Raise error if too high
+            lowHigh = "high"
+            raise ValueError
+        firstSeats = option
+        standardSeats = aircraft[3] - firstSeats * 2
+        print("No. of 1st class seats: " + str(firstSeats))
+        print("No. of standard seats: " + str(standardSeats))
+    except ValueError: # Handle too low or high
+        print("Value entered was too " + lowHigh + ". Returning to menu.")
+    except: # Handle other exceptions
         print("Input was invalid. Returning to menu.")
 
 
 while True:
-    loopState = mainMenu()
+    loopState = mainMenu() # Run the menu (which runs the other options)
     if loopState == "quit":
-        break
-    sleep(1.5)
+        break # Break out of the loop (to quit, see below)
+    sleep(1.5) # Add pause so text can be read more easily
 
 try:
     quit()  # Quit by raising SystemExit
